9f3acda0346d2d49a0bdffbd6a51599c0f3b5c9d
NickRylander <nick.rylander@outlook.com>
2020-10-06 14:33:26 -0700

    added sessions controller and view folder

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,15 +13,37 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
 
+  # helpers do
+  #   def currect_user
+  #     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  #     # if @current_user is nil, set User.find(...) if there is a session
+  #   end
+
+  #   def logged_in?  # forced into a boolean value as indicatied by the '?'
+  #     !!current_user
+  #     # Is the vairable set? If so, evaluate true, if not, evaluate false. The double negation forces the return value of a boolean
+  #   end
+  # end
+
   helpers do
-    def currect_user
-      @current_user ||= User.find(session[:user_id]) if session[:user_id]
-      # if @current_user is nil, set User.find(...) if there is a session
+    def logged_in?
+      session[:user_id]
     end
 
-    def logged_in?  # forced into a boolean value as indicatied by the '?'
-      !!current_user
-      # Is the vairable set? If so, evaluate true, if not, evaluate false. The double negation forces the return value of a boolean
+    def current_user
+      @user ||= User.find_by(id: session[:user_id])
+    end
+
+    def redirect_if_not_logged_in
+      if !logged_in?
+        redirect "/login"
+      end
+    end
+
+    def redirect_if_logged_in
+      if logged_in?
+        redirect "/discs"
+      end
     end
   end
 end
--- app/controllers/discs_controller.rb
+++ app/controllers/discs_controller.rb
@@ -1,30 +1,30 @@
 class DiscsController < ApplicationController
 
-    get '/discs' do
+    get '/discs' do # displays a list of all discs
         erb :index
     end
 
-    get '/discs/new' do
+    get '/discs/new' do # displays a from to make and save another disc
         
     end
 
-    get '/discs/:id' do
+    get '/discs/:id' do # displays one insance of a disc
         
     end
 
-    get '/discs/:id/edit' do
+    get '/discs/:id/edit' do # displays a form to edit a disc
         
     end
 
-    post '/discs' do
+    post '/discs' do # creates an instance of a new disc
         
     end
 
-    patch '/discs/:id' do
+    patch '/discs/:id' do # updates an instance of a disc
         
     end
 
-    delete '/discs/:id' do
+    delete '/discs/:id' do # deletes an isntance of a disc
         
     end
 end
