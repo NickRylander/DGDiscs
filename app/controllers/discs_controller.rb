fc2506e319249819df4abfd63ef9e08536c29d12
NickRylander <nick.rylander@outlook.com>
2020-10-06 12:02:18 -0700

    added disc and users folders in views

--- app/controllers/discs_controller.rb
+++ app/controllers/discs_controller.rb
@@ -1,3 +1,30 @@
 class DiscsController < ApplicationController
 
+    get '/discs' do
+        erb :index
+    end
+
+    get '/discs/new' do
+        
+    end
+
+    get '/discs/:id' do
+        
+    end
+
+    get '/discs/:id/edit' do
+        
+    end
+
+    post '/discs' do
+        
+    end
+
+    patch '/discs/:id' do
+        
+    end
+
+    delete '/discs/:id' do
+        
+    end
 end
