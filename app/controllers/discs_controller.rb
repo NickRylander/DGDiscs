979b2e24025e41aab82d1f7d3b3c1f4765ef34fa
NickRylander <nick.rylander@outlook.com>
2020-10-10 09:12:13 -0700

    new and show erb

--- app/controllers/discs_controller.rb
+++ app/controllers/discs_controller.rb
@@ -1,32 +1,43 @@
 class DiscsController < ApplicationController
 
     get '/discs' do # displays a list of all discs
+        # redirect_if_not_logged_in
         @discs = Disc.all # sets an instance variable to all the disc objects
         erb :"discs/index"
     end
 
     get '/discs/new' do # displays a from to make and save another disc
+        # redirect_if_not_logged_in
+        @users = User.all
         erb :"discs/new"
     end
 
     get '/discs/:id' do # displays one insance of a disc
-        
+        # redirect_if_not_logged_in
+        @disc = Disc.find_by_id(params[:id])
+        erb :"discs/show"
     end
 
     get '/discs/:id/edit' do # displays a form to edit a disc
+        # redirect_if_not_logged_in
         
     end
 
     post '/discs' do # creates an instance of a new disc
-        @discs = Disc.new(params) #sets all attributes
+        disc = Disc.new(params) #sets all attributes
+        if disc.save
+            redirect "/discs/#{disc.id}"
+        else
+            redirect "discs/new"
+        end
     end
 
     patch '/discs/:id' do # updates an instance of a disc
-        
+        @disc = Disc.find_by_id(params[:id])
     end
 
     delete '/discs/:id' do # deletes an isntance of a disc
-        
+        @disc = Disc.find_by_id(params[:id])
     end
 end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,11 @@
-5.times do
-    User.create(username: Faker::Internet.username, email: Faker::Internet.email,password: Faker::Internet.password)
+2.times do
+    User.create(username: Faker::Internet.username , email: Faker::Internet.email , password: Faker::Internet.password)
+end
+
+2.times do
+    Disc.create(brand: ["Innova", "Discraft"].sample , color: Faker::Color.color_name , speed: rand(1..13), glide: rand(1..7) , turn: rand(-5..1), fade: rand(0..5), user_id: 1, name: ["Destroyer", "Thunderbird", "Roc", "Sidewinder", "Katana", "Aviar", "Turn"].sample )
+end
+
+2.times do
+    Disc.create(brand: ["Innova", "Discraft"].sample , color: Faker::Color.color_name , speed: rand(1..13), glide: rand(1..7) , turn: rand(-5..1), fade: rand(0..5), user_id: 2, name: ["Destroyer", "Thunderbird", "Roc", "Sidewinder", "Katana", "Aviar", "Turn"].sample )
 end
